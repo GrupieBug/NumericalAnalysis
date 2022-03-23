d2d98ff23d112bb6b99cd04d9821ef924ed86706
GrupieBug <emkerr00@gmail.com>
2022-03-22 18:25:34 -0600

    EK: finished pt 1

--- Programming3/assignment3.py
+++ Programming3/assignment3.py
@@ -7,10 +7,12 @@ from matplotlib import pyplot as plt
 def basis(x_vals, degree, h):
     mat = np.ones((x_vals.shape[-1], degree + 1))
     for i in range(0, degree):
-        # Each column is equal to the previous column times xvals.
         mat[:, i+1] = (mat[:, i] * x_vals)
+
+    # we're dealing with the transpose of the monomial matrix
     np.transpose(mat)
 
+    # we divide each index by h except first column
     for j in range(1, len(x_vals)):
         mat[:, j] = mat[:, j] / h
     return mat
@@ -39,23 +41,37 @@ def f(x): return np.sin(x)
 
 # def f(x): return 1.0 / (1.0 + 25.0 * (x - 1.5)**2.0)
 def main():
-    n = 10
-    h = 1
-    # Get equally spaced points
-    # xvals = np.linspace(0, 3, n + 1)
-    xvals = np.linspace(0, 2 * math.pi, n+1)
-    for i in range(0, n):
-        xvals[i] + (h * i)
+    h_vals = np.linspace(1, .0001, 50)
+    error = []
+    for j in range(0, len(h_vals)):
+        n = 4  # number of pts to evaluate. we are told to evaluate  ˆx, ˆx+h, ˆx+2h, ˆx+3h, which is 4 pts
+        h = h_vals[j]
 
-    # yvals = f(xvals)
-    basis_primes = np.zeros(n + 1)
-    basis_primes[2] = 2/h
-    weights = find_weights(xvals, basis_primes, h)
+        x_vals = np.linspace(0, 2 * math.pi, n+1)
 
-    # Evaluate the polynomial
-    # ask prof
-    eval_pts = np.linspace(0, 3, 10)
-    pvals = evaluate_polynomials(weights, eval_pts, h)
+        # calculating x pts based on x-hats given
+        for i in range(0, n):
+            x_vals[i] + (h * i)
+
+        y_vals = f(x_vals)
+        basis_primes = np.zeros(n + 1)
+        basis_primes[2] = 2/h
+        weights = find_weights(x_vals, basis_primes, h)
+
+        approximation_sum = 0
+        for k in range(0, len(weights)):
+            approximation_sum = weights[k] * y_vals[k]
+
+        f_double_prime = np.diff(np.diff(f(x_vals)))
+        error.append(f_double_prime[0] - approximation_sum)  # calculating error for this particular h value and
+        # appending it to the list of our errors
+
+    plt.plot(h_vals, error, linewidth=4, label='Error')
+    plt.xlabel('h')
+    plt.ylabel('error')
+    plt.legend()
+    plt.title("Error as h -> 0")
+    plt.show()
 
 
 if __name__ == "__main__":
